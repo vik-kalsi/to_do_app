3e086aa6f83fd638b0d8f1d0c056ec7f95b929c7
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2024-01-11 20:18:57 +0000

    Add files via upload

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -4,17 +4,15 @@ import { useRef } from 'react';
 function AddTask() {
 
     const TextField = useRef();
-    const test = useRef();
+
 
     const AddTaskToList = () => {
         console.log(TextField.current.value);
-        //test = TextField.current.value
         TextField.current.value = "";
     }
 
     return(  
     <>
-        <p ref={test}>Check it out: </p>
         <input ref={TextField} type="text" placeholder="Add your task" className="p-2 mt-8 font-bold rounded-2xl text-black"></input>
         <button onClick={AddTaskToList} type="button" className="mx-4 border-2 font-semibold text-white rounded-2xl p-2">Add Task</button>
     </>
